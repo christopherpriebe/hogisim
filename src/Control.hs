9dcc8638470fbbe7ddfd044d20bac98ee4297be7
Christopher Priebe <chrisp9090@gmail.com>
2022-12-06 14:47:56 -0800

    Refactored Control module
    Easier to understand now and more abstraction

--- src/Control.hs
+++ src/Control.hs
@@ -1,52 +1,59 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Control where
 
-import Data.Matrix as M --TODO: Delete
 import Brick as B
-import Brick.Widgets.List as BWL
 import Graphics.Vty as V
 
-import Model
-import Model.Cell as C --TODO: Delete
+import Model as Model
 import Types
 
-handleEvent :: AppState -> B.BrickEvent String Name -> B.EventM Name (Next AppState)
-handleEvent (Menu New) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue (Menu Exit)
-handleEvent (Menu Load) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue (Menu New)
-handleEvent (Menu Exit) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue (Menu Load)
-handleEvent (Menu New) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue (Menu Load)
-handleEvent (Menu Load) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue (Menu Exit)
-handleEvent (Menu Exit) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue (Menu New)
+handleEvent :: Model.AppState -> B.BrickEvent String Name -> B.EventM Name (Next Model.AppState)
+handleEvent (Model.Menu m) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue
+  case m of
+    Model.New -> Model.Menu Model.Exit
+    Model.Load -> Model.Menu Model.New
+    Model.Exit -> Model.Menu Model.Load
+handleEvent (Model.Menu m) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue
+  case m of
+    Model.New -> Model.Menu Model.Load
+    Model.Load -> Model.Menu Model.Exit
+    Model.Exit -> Model.Menu Model.New
 
-handleEvent (Menu New) (B.VtyEvent (V.EvKey V.KEnter [])) = B.continue (Work initWorkState)
-handleEvent (Menu Load) (B.VtyEvent (V.EvKey V.KEnter [])) = B.suspendAndResume $ do
+handleEvent (Model.Menu Model.New) (B.VtyEvent (V.EvKey V.KEnter [])) = B.continue (Model.Work initWorkState)
+handleEvent (Model.Menu Model.Load) (B.VtyEvent (V.EvKey V.KEnter [])) = B.suspendAndResume $ do
                                                                                     b <-readFile "out.hgs"
-                                                                                    return (Work initWorkState)
+                                                                                    return (Model.Work initWorkState)
                                                                                     -- let loadedWorkState = WS {mode = View, board = b::Board, cursorPos = initCursorPos, editList = initEditList}
                                                                                     -- return (Work loadedWorkState)
-handleEvent (Menu Exit) (B.VtyEvent (V.EvKey V.KEnter [])) = B.halt (Menu Exit)
+handleEvent (Model.Menu Model.Exit) (B.VtyEvent (V.EvKey V.KEnter [])) = B.halt (Model.Menu Model.Exit)
 
-handleEvent (Work (WS { mode = View, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey (V.KChar '2') [])) = B.continue (Work (WS { mode = Edit, board = b, cursorPos = cp, editList = el }))
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey (V.KChar '1') [])) = B.continue (Work (WS { mode = View, board = b, cursorPos = cp, editList = el }))
-handleEvent (Work (WS { mode = View, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey (V.KChar '3') [])) = B.continue (Work (WS { mode = Run, board = b, cursorPos = cp, editList = el }))
-handleEvent (Work (WS { mode = Run, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey (V.KChar '1') [])) = B.continue (Work (WS { mode = View, board = b, cursorPos = cp, editList = el }))
-
-handleEvent (Work (WS { mode = m, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey V.KRight [])) = B.continue (Work (WS { mode = m, board = b, cursorPos = cursorRight cp, editList = el }))
-handleEvent (Work (WS { mode = m, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey V.KLeft [])) = B.continue (Work (WS { mode = m, board = b, cursorPos = cursorLeft cp, editList = el }))
-handleEvent (Work (WS { mode = m, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue (Work (WS { mode = m, board = b, cursorPos = cursorUp cp, editList = el }))
-handleEvent (Work (WS { mode = m, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue (Work (WS { mode = m, board = b, cursorPos = cursorDown cp, editList = el }))
-
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey V.KUp [V.MShift])) = B.continue (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = BWL.listMoveBy (-1) el }))
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey V.KDown [V.MShift])) = B.continue (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = BWL.listMoveBy 1 el }))
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey (V.KChar 'f') [])) = B.continue (Work (WS { mode = Edit, board = M.setElem (C.setContent (M.getElem y x b) (getListSelectedElement el)) (y, x) b, cursorPos = (y, x), editList = el }))
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey (V.KChar 'r') [])) = B.continue (Work (WS { mode = Edit, board = M.setElem (C.rotate (M.getElem y x b)) (y, x) b, cursorPos = (y, x), editList = el }))
-handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey (V.KChar 'd') [])) = B.continue (Work (WS { mode = Edit, board = M.setElem (C.setContent (M.getElem y x b) C.Empty) (y, x) b, cursorPos = (y, x), editList = el }))
-
-handleEvent (Work (WS { mode = View, board = b, cursorPos = cp, editList = el })) (B.VtyEvent (V.EvKey (V.KChar 's') [])) = B.suspendAndResume $ do
-                                                                                                                                                    writeFile "out.hgs" (show b)
-                                                                                                                                                    return (Work (WS { mode = View, board = b, cursorPos = cp, editList = el }))
---handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey V.KUp [V.MShift])) = B.continue (Work (WS { mode = Edit, board = M.setElem (C.nextContentForward (M.getElem y x b)) (y, x) b, cursorPos = (y, x), editList = el }))
---handleEvent (Work (WS { mode = Edit, board = b, cursorPos = (y, x), editList = el })) (B.VtyEvent (V.EvKey V.KDown [V.MShift])) = B.continue (Work (WS { mode = Edit, board = M.setElem (C.nextContentBackward (M.getElem y x b)) (y, x) b, cursorPos = (y, x), editList = el }))
-
+handleEvent (Model.Work ws) (B.VtyEvent (V.EvKey V.KRight [])) = B.continue (Model.Work (Model.moveCursorPosRight ws))
+handleEvent (Model.Work ws) (B.VtyEvent (V.EvKey V.KLeft [])) = B.continue (Model.Work (Model.moveCursorPosLeft ws))
+handleEvent (Model.Work ws) (B.VtyEvent (V.EvKey V.KUp [])) = B.continue (Model.Work (Model.moveCursorPosUp ws))
+handleEvent (Model.Work ws) (B.VtyEvent (V.EvKey V.KDown [])) = B.continue (Model.Work (Model.moveCursorPosDown ws))
 handleEvent (Work _) (B.VtyEvent (V.EvKey V.KEsc [])) = B.continue (Menu New)
+handleEvent (Model.Work ws) e =
+  case (getMode ws) of
+    Model.View -> handleViewEvent ws e
+    Model.Edit -> handleEditEvent ws e
+    Model.Run -> handleRunEvent ws e
 
-handleEvent s _ = B.continue s
+handleViewEvent :: Model.WorkState -> B.BrickEvent String Name -> B.EventM Name (Next Model.AppState)
+handleViewEvent ws (B.VtyEvent (V.EvKey (V.KChar '2') [])) = B.continue (Model.Work (Model.setMode ws Model.Edit))
+handleViewEvent ws (B.VtyEvent (V.EvKey (V.KChar '3') [])) = B.continue (Model.Work (Model.setMode ws Model.Run))
+handleViewEvent ws (B.VtyEvent (V.EvKey (V.KChar 's') [])) = B.suspendAndResume (do {writeFile "out.hgs" (show (Model.getBoard ws)); return (Model.Work ws)})
+handleViewEvent ws _ = B.continue (Model.Work ws)
+
+handleEditEvent :: Model.WorkState -> B.BrickEvent String Name -> B.EventM Name (Next Model.AppState)
+handleEditEvent ws (B.VtyEvent (V.EvKey (V.KChar '1') [])) = B.continue (Model.Work (Model.setMode ws Model.View))
+handleEditEvent ws (B.VtyEvent (V.EvKey V.KUp [V.MShift])) = B.continue (Model.Work (Model.moveEditListCursorUp ws))
+handleEditEvent ws (B.VtyEvent (V.EvKey V.KDown [V.MShift])) = B.continue (Model.Work (Model.moveEditListCursorDown ws))
+handleEditEvent ws (B.VtyEvent (V.EvKey (V.KChar 'f') [])) = B.continue (Model.Work (Model.setCellToSelectedEditListCell ws))
+handleEditEvent ws (B.VtyEvent (V.EvKey (V.KChar 'r') [])) = B.continue (Model.Work (Model.setCellToRotate ws))
+handleEditEvent ws (B.VtyEvent (V.EvKey (V.KChar 'd') [])) = B.continue (Model.Work (Model.setCellToEmpty ws))
+handleEditEvent ws _ = B.continue (Model.Work ws)
+
+handleRunEvent :: Model.WorkState -> B.BrickEvent String Name -> B.EventM Name (Next Model.AppState)
+handleRunEvent ws (B.VtyEvent (V.EvKey (V.KChar '1') [])) = B.continue (Model.Work (Model.setMode ws Model.View))
+handleRunEvent ws _ = B.continue (Model.Work ws)
--- src/Model.hs
+++ src/Model.hs
@@ -7,6 +7,7 @@ import Brick.Widgets.List as BWL
 
 import Types
 import Model.Cell as C
+import Sim as S
 
 data AppState
   = Menu MenuState
@@ -29,6 +30,98 @@ data WorkState = WS
   , cursorPos :: Coordinate
   , editList :: BWL.GenericList String V.Vector (C.CellContent, Name)
   }
+
+getMode :: WorkState -> ModeState
+getMode ws = mode ws
+
+getBoard :: WorkState -> Board
+getBoard ws = board ws
+
+getCursorPos :: WorkState -> Coordinate
+getCursorPos ws = cursorPos ws
+
+getEditList :: WorkState -> BWL.GenericList String V.Vector (C.CellContent, Name)
+getEditList ws = editList ws
+
+setMode :: WorkState -> ModeState -> WorkState
+setMode ws ms = WS
+  { mode = ms
+  , board = getBoard ws
+  , cursorPos = getCursorPos ws
+  , editList = getEditList ws
+  }
+
+setBoard :: WorkState -> Board -> WorkState
+setBoard ws b = WS
+  { mode = getMode ws
+  , board = b
+  , cursorPos = getCursorPos ws
+  , editList = getEditList ws
+  }
+
+setCursorPos :: WorkState -> Coordinate -> WorkState
+setCursorPos ws cp = WS
+  { mode = getMode ws
+  , board = getBoard ws
+  , cursorPos = cp
+  , editList = getEditList ws
+  }
+
+setEditList :: WorkState -> BWL.GenericList String V.Vector (C.CellContent, Name) -> WorkState
+setEditList ws el = WS
+  { mode = getMode ws
+  , board = getBoard ws
+  , cursorPos = getCursorPos ws
+  , editList = el
+  }
+
+setCellAtCursorPosWithCell :: WorkState -> C.CellContent -> WorkState
+setCellAtCursorPosWithCell ws cc = setBoard ws (M.setElem (C.setContent (M.getElem y x b) cc) (y, x) b)
+  where
+    b = getBoard ws
+    (y, x) = getCursorPos ws
+
+setCellToSelectedEditListCell :: WorkState -> WorkState
+setCellToSelectedEditListCell ws = setCellAtCursorPosWithCell ws selectedCell
+  where
+    selectedCell = getEditListSelectedCell ws
+
+setCellToEmpty :: WorkState -> WorkState
+setCellToEmpty ws = setCellAtCursorPosWithCell ws C.Empty
+
+setCellToRotate :: WorkState -> WorkState
+setCellToRotate ws = setBoard ws (M.setElem (C.rotate currCell) (y, x) b)
+  where
+    b = getBoard ws
+    (y, x) = getCursorPos ws
+    currCell = M.getElem y x b
+
+moveCursorPosRight :: WorkState -> WorkState
+moveCursorPosRight ws = moveCursorPos ws cursorRight
+
+moveCursorPosLeft :: WorkState -> WorkState
+moveCursorPosLeft ws = moveCursorPos ws cursorLeft
+
+moveCursorPosUp :: WorkState -> WorkState
+moveCursorPosUp ws = moveCursorPos ws cursorUp
+
+moveCursorPosDown :: WorkState -> WorkState
+moveCursorPosDown ws = moveCursorPos ws cursorDown
+
+moveCursorPos :: WorkState -> (Coordinate -> Coordinate) -> WorkState
+moveCursorPos ws f = setCursorPos ws (f (getCursorPos ws))
+
+moveEditListCursorUp :: WorkState -> WorkState
+moveEditListCursorUp ws = setEditList ws (BWL.listMoveBy (-1) (getEditList ws))
+
+moveEditListCursorDown :: WorkState -> WorkState
+moveEditListCursorDown ws = setEditList ws (BWL.listMoveBy (1) (getEditList ws))
+
+getEditListSelectedCell :: WorkState -> C.CellContent
+getEditListSelectedCell ws =
+  case BWL.listSelectedElement (getEditList ws) of
+    Just tup -> fst (snd tup)
+    Nothing -> C.Empty
 
 initWorkState :: WorkState
 initWorkState = WS
@@ -62,12 +155,6 @@ cursorDown (y, x) = if y < boardSize  then (y + 1, x) else (y, x)
 
 initCursorPos :: Coordinate
 initCursorPos = (div boardSize 2, div boardSize 2)
-
-getListSelectedElement :: BWL.GenericList String V.Vector (C.CellContent, Name) -> C.CellContent
-getListSelectedElement el =
-  case BWL.listSelectedElement el of
-    Just tup -> fst (snd tup)
-    Nothing -> C.Empty
 
 initEditList :: BWL.GenericList String V.Vector (C.CellContent, Name)
 initEditList = BWL.list "editList" (C.placeableCellContents) 1
