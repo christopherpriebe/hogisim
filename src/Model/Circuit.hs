eeea52a74a0339c570bbd716d6dc73338cf0b43d
Shyama Shastri <sshastri@ucsd.edu>
2022-12-06 14:48:27 -0800

    everything i have so far

--- src/Model/Circuit.hs
+++ src/Model/Circuit.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Model.Circuit where
 {--
 A prototype representation of connectable gate components
@@ -13,65 +17,86 @@ https://media.geeksforgeeks.org/wp-content/uploads/3-57.png
 {-#LANGUAGE arrows #-}
 {-# LANGUAGE RankNTypes #-}
 import FRP.Yampa
-import Data.Set
-import Data.HashMap.Strict
-import Algebra.Graph
+
+--import Data.HashMap.Strict
+--import Algebra.Graph
+import Algebra.Graph.AdjacencyMap
+import Algebra.Graph.AdjacencyMap.Algorithm
 import Data.Set.Monad
+import Algebra.Graph.Class
+import Control.Monad
+import Control.Applicative (Applicative(liftA2))
+import Data.List
+import qualified Data.List.NonEmpty
+import FRP.Yampa.Arrow (arr2)
 --import Data.Lens
 
 
 --first number corresponds to the gate number, the second corresponds to the gate input number
 --type InputSet = HashMap
 type Input  = (Set (Int, Int))
-data Output = O Int (SF a Bool)
-data Node = N Int (Bool->Bool-> Bool) (forall a. SF a Bool)
-type NodeMap = NM AdjacencyIntMap (HashMap.Strict Int Node)
+data Output = O Int (forall a. SF a Bool)
+data Node = N Int (forall a. a-> Bool) (forall a. SF a Bool)
+--type NodeMap = NM AdjacencyMap (HashMap.Strict Int Node)
 
-emNG :: NodeMap
-emNG = empty
+--emNG :: NodeMap
+--emNG = empty
 
 data Circuit where
   C :: Set Input -> AdjacencyMap Node -> Set Output -> Circuit
 
-recalculate :: AdjacencyIntMap -> NodeMap -> NodeMap
+--recalculate :: AdjacencyIntMap -> NodeMap -> NodeMap
 
 instance Ord Node where
   (<=) (N i _ _) (N i' _ _) = i <= i'
 
-instance Graph (Circuit) where
-    type Vertex (Circuit) = Node
-    empty = (C (EP empty) empty empty (EP -1 (constant false)))
+instance Graph Circuit where
+    type Vertex Circuit = Node
+    --empty = C (EP empty) empty empty (EP -1 (constant false))
 
-    vertex (i, Node f stream) = C (fromList [(singleton (i,0)), singleton (i, 1) ]) 
-                                  (vertex i)
-                                  (insert i node empt)
-                                  (O i (arr f))
-    overlay (C si aim nm so) (C si' aim' nm' so') = C (mergeInputs si si') (aim + aim') (union so so')
+    vertex (N i f s) = C (fromList [singleton (i,0), singleton (i, 1) ])
+                                  (Algebra.Graph.AdjacencyMap.vertex (N i f (arr f)))
+                                  (singleton (O i (arr f)))
+    overlay (C si aim so) (C si' aim'  so') = C (mergeInputs si si') (aim + aim') (Data.Set.Monad.union so so')
       where
-        mergeInputs si si' = fromList (u ++ map (\s -> [a | a <- s, not (isSubsetOf a (join u))]) [si, si'])
-          where 
-            u = fromList [union a b | a <- si, b <- si', not $ disjoint a b]
-        graphCheck aim'' = (gmap (\n -> if member n diff then (recLoop n) else n) loops)
+        mergeInputs (si::Set Input) si' = Data.List.map (\s -> [a | a <- s, not (isSubsetOf a (join u))]) [si, si']
+          where
+            --b = ( Data.Set.Monad.map (\s -> [a | a <- s, not (isSubsetOf a (join u))]) (fromList [si, si']))
+            u = fromList [Data.Set.Monad.union a b | a <- si, b <- si', not $ disjoint a b]
+        graphCheck aim'' = gmap (\n -> if member n diff then recLoop n else n) loops
         --overlay <function turning new diff set into graph> (map recLoop diff) (scc aim) 
           where
-            loops = topSort . scc aim''
-            diff = (difference (vertexSet . loops) (vertexSet . scc aim))
+            loops = topSort (scc aim'')
+            diff = difference (vertexSet loops) (vertexSet . scc aim)
             -- <edge relation of merged graph with which to reconstruct merged graph with updated nodes>
 
-        recLoop g aim nodeSet = 
+        recLoop subgraph  nodeSet = if isAcyclic subgraph then return nodeSet else recLoop sbgrph' (c:nodeSet)
           where
-            s = vertexSet g
+
+            v = case topSort subgraph of
+              Right _ -> vertexList subgraph
+              Left  x -> Data.List.NonEmpty.toList x
+            e = edgeList subgraph
+            score inc out = abs (length inc - length out)
+            candidate =foldl1 (\ x@(z,_) y@(w,_) -> if z <= w then y else x)  edgecounts
+            c = candidate
+            mapNode y = Data.List.map (`y` subgraph)
+            edgecounts = zip (zipWith score (mapNode preSet v ) (mapNode postSet v)) e
+            edgeFunc sb (e1,e2) = removeEdge e1 e2 sb
+            sbgrph' = Data.List.foldl edgeFunc subgraph e
 
 
 
-        updateNodes aim'' = (do
-          n <- (vertexSet aim)
-          diffn <-difference (preset n aim'') (preset n aim)
-          return diffn) 
+
+
+
+        updateNodes aim'' = do
+          n <- vertexSet aim
+          difference (preset n aim'') (preset n aim)
           where
-            list = vertexList 
+            list = vertexList
 
-        
-        
-          
-    connect
+
+
+
+
