bcd6b40c6689af9240ce24db4e6001c9c265ed5c
Christopher Priebe <chrisp9090@gmail.com>
2022-12-06 17:14:50 -0800

    Framework for testing

--- src/View.hs
+++ src/View.hs
@@ -4,11 +4,11 @@ import Brick as B
 import Brick.Widgets.Border as BWB
 import Brick.Widgets.List as BWL
 import Graphics.Vty as V
-import Data.Matrix as M --TODO: Remove ASAP
-import Data.Vector --TODO: Remove ASAP
+import Data.Matrix as M
+import Data.Vector as Vec
 
 import Model
-import Model.Cell --Add rename
+import Model.Cell --TODO: add rename as C
 import Types
 
 --TODO: Implement way to pass argument from console to only render using ASCII for certain terminal compatability
@@ -24,22 +24,35 @@ view' (Menu ms) = case ms of
     menuNewWidget = B.str "New Workspace"
     menuLoadWidget = B.str "Load Workspace"
     menuExitWidget = B.str "Exit"
-view' (Work ws) = case (mode ws) of
-  View -> vboard <=> (B.str "View") <=> vcontr <+> (BWB.border (B.str "STATS HERE"))
-  Edit -> vboard <=> (B.str "Edit") <=> econtr <+> (B.hLimit 30 (B.vLimit (boardSize + 2) (viewEditList (editList ws))))
-  Run  -> vboard <=> (B.str "Run") <=> rcontr <+> (BWB.border (B.str "STATS HERE"))
+view' (Work ws) = case (getMode ws) of
+  View -> (vboard <=> (B.str "View") <=> vcontr) <+> (vboardRightBox <=> vconsole)
+  Edit -> (vboard <=> (B.str "Edit") <=> econtr) <+> (eboardRightBox <=> vconsole)
+  Run  -> (vboard <=> (B.str "Run") <=> rcontr) <+> (rboardRightBox <=> vconsole)
   where
     vboard = viewBoard (board ws) (cursorPos ws)
     vcontr = (B.str "Controls:") <=> (B.str "Exit to Menu - \"Esc.\"") <=> (B.str "Switch to \"Edit\" - \"2\"") <=> (B.str "Switch to \"Run\" - \"3\"") <=> (B.str "Move Cursor - (Up | Down | Left | Right) Arrow") <=> (B.str "Save Current View - \"s\" (File will be saved as \"out.hgs\")")
-    econtr = (B.str "Controls:") <=> (B.str "Exit to Menu - \"Esc.\"") <=> (B.str "Switch to \"View\" - \"1\"") <=> (B.str "Move Cursor - (Up | Down | Left | Right) Arrow") <=> (B.str "Change Selection - Shift+(Up Arrow | Down Arrow)") <=> (B.str "Place Selected Cell - \"f\"") <=> (B.str "Rotate Selected Cell - \"r\"")
+    econtr = (B.str "Controls:") <=> (B.str "Exit to Menu - \"Esc.\"") <=> (B.str "Switch to \"View\" - \"1\"") <=> (B.str "Move Cursor - (Up | Down | Left | Right) Arrow") <=> (B.str "Change Selection - Shift+(Up Arrow | Down Arrow)") <=> (B.str "Place Selected Cell - \"f\"") <=> (B.str "Clear Cell - \"d\"") <=> (B.str "Rotate Selected Cell - \"r\"")
     rcontr = (B.str "Controls:") <=> (B.str "Exit to Menu - \"Esc.\"") <=> (B.str "Switch to \"View\" - \"1\"") <=> (B.str "Move Cursor - (Up | Down | Left | Right) Arrow")
+    vboardRightBoxNoPad = B.str "stats here"
+    vboardRightBox = padRightBox vboardRightBoxNoPad
+    eboardRightBoxNoPad = viewEditList (editList ws)
+    eboardRightBox = padRightBox eboardRightBoxNoPad
+    rboardRightBoxNoPad = B.str "stats here"
+    rboardRightBox = padRightBox rboardRightBoxNoPad
+    vconsole = padConsole (B.vBox (Vec.toList (Vec.map (\s -> B.str s) (Model.getConsole ws))))
+
+padRightBox :: B.Widget Name -> B.Widget Name
+padRightBox w = BWB.border (B.hLimit 50 (B.vLimit boardSize (B.padRight B.Max (B.padBottom B.Max w))))
+
+padConsole :: B.Widget Name -> B.Widget Name
+padConsole w = BWB.border (B.hLimit 50 (B.vLimit 5 (B.padRight B.Max (B.padTop B.Max w))))
 
 viewBoard :: Board -> Coordinate -> Widget Name
 viewBoard b cp = (BWB.border (viewBoardNoBorder b cp))
 
 -- TODO: Modify these functions so Data.Vector is no longer needed
 viewBoardNoBorder :: Board -> Coordinate -> Widget Name
-viewBoardNoBorder b cp = Prelude.foldl (\acc i -> acc <=> (Data.Vector.foldl1 (<+>) (M.getRow i wb))) (Data.Vector.foldl1 (<+>) (M.getRow 1 wb)) [2..boardSize] --TODO: Fix potential bug with board sizes of 0
+viewBoardNoBorder b cp = Prelude.foldl (\acc i -> acc <=> (Vec.foldl1 (<+>) (M.getRow i wb))) (Vec.foldl1 (<+>) (M.getRow 1 wb)) [2..boardSize] --TODO: Fix potential bug with board sizes of 0
   where
     wb = (toWidgetBoard b cp)
 
@@ -48,7 +61,7 @@ toWidgetBoard b cp = fmap (\c -> if (coordinate c) == cp then B.withAttr select 
 --End TODO
 
 viewEditList :: BWL.GenericList String Vector (CellContent, Name) -> Widget Name
-viewEditList el = BWB.border (BWL.renderList (\b tup -> if b then B.withAttr select (viewCellContentAndName tup) else viewCellContentAndName tup) False el)
+viewEditList el = BWL.renderList (\b tup -> if b then B.withAttr select (viewCellContentAndName tup) else viewCellContentAndName tup) False el
 
 viewCellContentAndName :: (CellContent, Name) -> Widget Name
 viewCellContentAndName (cc, n) = (B.str (toString' cc)) <+> (B.str " - ") <+> (B.str n)
--- tests/Test.hs
+++ tests/Test.hs
@@ -1,9 +1,35 @@
 module Main where
 
-import System.Exit
+import Test.HUnit
+
+import Data.Vector as V
+import Data.Matrix as M
+
+import Types
+import Model
+import Model.Cell
+import Sim
 
 main :: IO ()
-main = do
-  putStrLn "\nRunning tests... "
-  putStrLn "\nDone Testing"
-  exitWith ExitSuccess
+main = runTestTTAndExit tests
+
+tests = TestList
+  [ TestLabel "emptyTransformEmptyBoard" testTransformEmptyBoard
+  ]
+
+testTransformEmptyBoard = TestCase (assertEqual "transform w/ Empty Board Failed" (transform b) [])
+  where
+    b = toBoard empty5x5
+
+toBoard :: [[CellContent]] -> Board
+toBoard ll = M.matrix size size (\(y, x) -> C { content = (ll!!y)!!x, coordinate = (y + 1, x + 1) })
+  where
+    size = Prelude.length ll
+
+--ALL BOARDS SQUARES
+empty5x5 = [ [Empty, Empty, Empty, Empty, Empty]
+           , [Empty, Empty, Empty, Empty, Empty]
+           , [Empty, Empty, Empty, Empty, Empty]
+           , [Empty, Empty, Empty, Empty, Empty]
+           , [Empty, Empty, Empty, Empty, Empty]
+           ]
